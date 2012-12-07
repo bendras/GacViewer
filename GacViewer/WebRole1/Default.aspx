1f41e41ba3d734197a64b1752aaeb9f14aba33c9
Paulius Zaliaduonis <bendras11@yahoo.com>
2012-12-07 14:58:59 +0000

    Working web app
    Enjoy it. Note that you might have to add custom azure configuration.

---
+++ GacViewer/WebRole1/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebRole1._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Windows Azure GAC Viewer</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox runat="server" Width="200" ID="assemblyName" />
+        <asp:Button runat="server" OnClick="OnViewAssemblyButtonClick" ID="viewAssemblyButton" Text="Look up Assembly"  />
+        <asp:Button runat="server" OnClick="OnViewAllButtonClick" ID="viewAllButton" Text="View all Assemblies"/>
+        <br />
+        <asp:TextBox runat="server" ID="ressultsBox" Width="600" Height="800" TextMode="MultiLine"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ GacViewer/WebRole1/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace WebRole1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OnViewAllButtonClick(object sender, EventArgs e)
+        {
+            string path = MapPath("gacutil.exe");
+            string command = "/l";
+
+            ressultsBox.Text = RunCommandLineTool(path, command);
+        }
+
+        protected void OnViewAssemblyButtonClick(object sender, EventArgs e)
+        {
+            string path = MapPath("gacutil.exe");
+            string command = "/l " + assemblyName.Text;
+
+            ressultsBox.Text = RunCommandLineTool(path, command);
+        }
+
+        private string RunCommandLineTool(string path, string parameters)
+        {
+            ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(path, parameters);
+            info.UseShellExecute = false;
+            info.ErrorDialog = false;
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+
+            Process p = Process.Start(info);
+
+            StreamReader sr = p.StandardOutput;
+            string output = sr.ReadToEnd();
+            sr.Close();
+
+            return output;
+        }
+    }
+}
